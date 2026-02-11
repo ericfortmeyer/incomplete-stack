c9bd0c251ae35ef292992924ef3d363eb13c14e7
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2026-02-10 23:21:30 -0500

    fix(hiram): add missing sources files
    Signed-off-by: Eric Fortmeyer <e.fortmeyer01@gmail.com>

--- hosts/hiram/init/20_apt_repos.sh
+++ hosts/hiram/init/20_apt_repos.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
 CFG_DIR="${REPO_ROOT}/hosts/hiram/config"
+PREV_APT_PACKAGES=docs/frankenstein/apt-manual.txt
 
 echo "[20_apt_repos] Ensuring /etc/apt/sources.list.d exists..."
 sudo mkdir -p /etc/apt/sources.list.d
@@ -17,15 +18,30 @@ if [[ ! -f /etc/apt/keyrings/microsoft.gpg ]]; then
   sudo chmod 0644 /etc/apt/keyrings/microsoft.gpg
 fi
 
-if [[ -f "${CFG_DIR}/apt-sources/vscode.sources" ]]; then
-  echo "[20_apt_repos] Installing VS Code sources file..."
-  sudo install -m 0644 "${CFG_DIR}/apt-sources/vscode.sources" /etc/apt/sources.list.d/vscode.sources
-else
-  echo "[20_apt_repos] Missing ${CFG_DIR}/apt-sources/vscode.sources"
-  exit 1
+if [[ ! -f /etc/apt/keyrings/sury-php.gpg ]]; then
+  echo "[20_apt_repos] Installing PHP GPG key..."
+  sudo curl -fsSL https://packages.sury.org/php/apt.gpg \
+    | sudo tee /etc/apt/keyrings/sury-php.gpg >/dev/null
+  sudo chmod 0644 /etc/apt/keyrings/sury-php.gpg
 fi
 
+if [[ ! -f /etc/apt/keyrings/slack.gpg ]]; then
+  echo "[20_apt_repos] Installing Slack GPG key..."
+  sudo install -d -m 0755 /etc/apt/keyrings
+  curl -fsSL https://packagecloud.io/slacktechnologies/slack/gpgkey \
+    | sudo tee /etc/apt/keyrings/slack.gpg >/dev/null
+  sudo chmod 0644 /etc/apt/keyrings/slack.gpg
+fi
+
+for source_path in "${CFG_DIR}/*.sources"; do
+    src=$(basename $source_path)
+    echo "[20_apt_repos] Installing ${src} file..."
+    sudo install -m 0644 "${CFG_DIR}/apt-sources/${src}" /etc/apt/sources.list.d/${src}
+done
+
+
 sudo apt update
-xargs -a docs/frankenstein/apt-manual.txt sudo apt install -y
+sudo apt install debian-archive-keyring
+xargs -a "$PREV_APT_PACKAGES"  sudo apt install -y
 
 echo "[20_apt_repos] Done."
