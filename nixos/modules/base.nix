ee3a41f0e05375a5afef94b6b587204d7b54f717
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2026-02-14 23:11:13 -0500

    fix(godel): configure system state, include git
    Signed-off-by: Eric Fortmeyer <e.fortmeyer01@gmail.com>

--- nixos/modules/base.nix
+++ nixos/modules/base.nix
@@ -2,6 +2,11 @@
 {
   time.timeZone = "America/Denver";
   i18n.defaultLocale = "en_US.UTF-8";
+  system.stateVersion = "25.11";
+
+  environment.systemPackages = with pkgs; [
+    git
+  ];
 
   # Enable flakes & new CLI inside Nix
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
