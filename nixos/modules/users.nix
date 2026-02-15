538991ebd95507ed526009136eccd12dbaf18636
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2026-02-14 23:09:22 -0500

    fix(godel): make users.nix corrections
    Signed-off-by: Eric Fortmeyer <e.fortmeyer01@gmail.com>

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -4,14 +4,16 @@
 
   users.users.efortmeyer = {
     isNormalUser = true;
-    group        = true;
+    group        = "users";
     extraGroups  = [ "wheel" "networkmanager" "docker" ];
     shell        = pkgs.zsh;
     openssh.authorizedKeys.keyFiles = [
-      projectRoot + /hosts/godel/authorized_keys/efortmeyer_godel_ed25519.pub
+      (projectRoot + /hosts/godel/authorized_keys/efortmeyer_godel_ed25519.pub)
+      (projectRoot + /hosts/godel/authorized_keys/manna_godel_ed25519.pub)
     ];
   };
 
+  programs.zsh.enable = true;
 
   security.sudo.wheelNeedsPassword = false;
 }
