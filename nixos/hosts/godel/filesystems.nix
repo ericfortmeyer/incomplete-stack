36657a89fff78495c786a8efec012f3dbbd381e6
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2026-06-29 22:34:42 -0400

    feat(godel): mount cold storage
    The 1TB HDD will be used for a private Docker registry and to store build caches.
    
    Signed-off-by: Eric Fortmeyer <e.fortmeyer01@gmail.com>

--- nixos/hosts/godel/filesystems.nix
+++ nixos/hosts/godel/filesystems.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 
 {
+  # Encrypted SSD Unlock
   fileSystems."/" = {
     device = "/dev/mapper/cryptroot";
     fsType = "btrfs";
@@ -16,5 +17,30 @@
     device = "/dev/mapper/cryptroot";
     fsType = "btrfs";
     options = [ "subvol=@home" ];
+  };
+
+  # Encrypted HDD unlock (automatic, no password prompt)
+  systemd.cryptsetup.hdd-storage = {
+    device = "/dev/sdb5";
+    keyFile = "/root/.luks-keys/hdd-cold-storage.key";
+  };
+
+  # Mount btrfs subvolumes
+  fileSystems."/mnt/hdd-registry" = {
+    device = "/dev/mapper/hdd-storage";
+    fsType = "btrfs";
+    options = [ "subvol=@registry" "compress=zstd:3" "noatime" ];
+  };
+
+  fileSystems."/mnt/hdd-artifacts" = {
+    device = "/dev/mapper/hdd-storage";
+    fsType = "btrfs";
+    options = [ "subvol=@artifacts" "compress=zstd:3" "noatime" ];
+  };
+
+  fileSystems."/mnt/hdd-scratch" = {
+    device = "/dev/mapper/hdd-storage";
+    fsType = "btrfs";
+    options = [ "subvol=@scratch" "compress=zstd:3" "noatime" ];
   };
 }
